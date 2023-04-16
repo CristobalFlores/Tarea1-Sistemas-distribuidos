b72621bf48e12ae80b5d6f7b480b1e5b2451d5ba
Cristobal Flores <52360895+CristobalFlores@users.noreply.github.com>
2023-04-15 20:30:00 -0400

    Update app.py

--- Sis Distribuidos (1)/Sis Distribuidos/backend/app.py
+++ Sis Distribuidos (1)/Sis Distribuidos/backend/app.py
@@ -17,14 +17,17 @@ client3 = redis.Redis(host='localhost', port=6381)  # Conexión a Redis 3
 
 # Función para ejecutar la consulta a la API y actualizar el caché en Redis
 def update_cache():
-    dato1 = str(client1.get("item"))[2:-1]
-    dato2 = str(client2.get("item"))[2:-1]
-    dato3 = str(client3.get("item"))[2:-1]
-    
-    #threading.Timer(10.0, update_cache).start()  # Ejecutar la función cada 10 segundos
-    do_query(dato1)
-    do_query(dato2)
-    do_query(dato3)
+    i=0
+    while i<1667:
+        i=i+1
+        dato1 = str(client1.get("item"))[2:-1]
+        dato2 = str(client2.get("item"))[2:-1]
+        dato3 = str(client3.get("item"))[2:-1]
+        
+        #threading.Timer(10.0, update_cache).start()  # Ejecutar la función cada 10 segundos
+        do_query(dato1)
+        do_query(dato2)
+        do_query(dato3)
          
 
 # Función para realizar la consulta a la API y actualizar el caché en Redis
@@ -34,45 +37,45 @@ def do_query(dato):
     
     
     if client1.hexists(dato,0):
-        print (client1.hget(dato,0))
+        #print (client1.hget(dato,0))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client1.hexists(dato,1):
-        print (client1.hget(dato,1))
+        #print (client1.hget(dato,1))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client1.hexists(dato,2):
-        print (client1.hget(dato,2))
+        #print (client1.hget(dato,2))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client2.hexists(dato,3):
-        print (client2.hget(dato,3))
+        #print (client2.hget(dato,3))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client2.hexists(dato,4):
-        print (client2.hget(dato,4))
+        #print (client2.hget(dato,4))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client2.hexists(dato,5):
-        print (client2.hget(dato,5))
+        #print (client2.hget(dato,5))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client3.hexists(dato,6):
-        print (client3.hget(dato,6))
+        #print (client3.hget(dato,6))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client3.hexists(dato,7):
-        print (client3.hget(dato,7))
+        #print (client3.hget(dato,7))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client3.hexists(dato,8):
-        print (client3.hget(dato,8))
+        #print (client3.hget(dato,8))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
     elif client3.hexists(dato,9):
-        print (client3.hget(dato,9))
+        #print (client3.hget(dato,9))
         elapsed_time = time.time() - start_time
-        print(f'Tiempo de consulta: {elapsed_time} segundos')
+        #print(f'Tiene elemento-Tiempo de consulta: {elapsed_time} segundos')
 
     else:
 
@@ -80,12 +83,12 @@ def do_query(dato):
             response = requests.get('https://api.archivelab.org/items/'+dato)  # Realizar consulta a la API
             data = response.json()
             #created = response.get('created')
-            print (data)
+            #print (data)
             item_id= str(data.get('created'))
             id = int(item_id[-1:])
 
 
-            print (response)
+            #print (response)
 
             if id >= 0 and id <= 2:
                 client1.hset(dato, id, json.dumps(data))
@@ -96,9 +99,10 @@ def do_query(dato):
             
             elapsed_time = time.time() - start_time
 
-            print(f'Tiempo de consulta: {elapsed_time} segundos')
-            print('Cache actualizado en Redis')
+            #print(f'No hay elemento Tiempo de consulta: {elapsed_time} segundos')
+            #print('Cache actualizado en Redis')
         except Exception as e:
+
             print(f'Error al realizar la consulta: {e}')
 
 
@@ -108,7 +112,7 @@ start_time = time.time()
 update_cache()
 
 elapsed_time = time.time() - start_time
-print(f'Tiempo de consulta: {elapsed_time} segundos')
+print(f'Update-Tiempo de consulta: {elapsed_time} segundos')
 
 # Ruta para realizar la consulta a la API
 @app.route('/consulta')
@@ -143,4 +147,3 @@ def consulta():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
-
